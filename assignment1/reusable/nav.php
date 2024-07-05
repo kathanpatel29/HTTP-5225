09ba3501e409e121e050a19b6ea6a7c91f98845a
kathanpatel29 <69028584+kathanpatel29@users.noreply.github.com>
2024-07-04 21:23:34 -0400

    Update nav.php

--- assignment1/reusable/nav.php
+++ assignment1/reusable/nav.php
@@ -1,13 +1,18 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="index.php">Aviation Models</a>
-    <div class="collapse navbar-collapse">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="add.php">Add Model</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="update.php">Update Model</a>
-            </li>
-        </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Aviation Models</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="add.php">Add Model</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="update.php">Update Model</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
